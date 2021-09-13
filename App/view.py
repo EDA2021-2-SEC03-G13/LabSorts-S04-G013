254a3f8667447db33a37dfaa810a2c59c6f54a6b
Santiago Rodriguez <s.rodriguez@uninandes.edu.co>
2021-09-13 14:37:40 -0500

    cambios 1

--- App/view.py
+++ App/view.py
@@ -92,11 +92,7 @@ def printSortResults(ord_books, sample=10):
             book = lt.getElement(ord_books,i)
             print('Titulo: ' + book['title'] + ' ISBN: ' + book['isbn'] + ' Rating: ' + book['average_rating'])
             i+=1
-    elif int(inputs[0]) == 5:
-        size = input("Indique tamaño de la muestra: ")
-        result = controller.sortBooks(catalog, int(size))
-        print("Para la muestra de", size, " elementos, el tiempo (mseg) es: ", str(result[0]))
-        printSortResults(result[1])
+    
 
 
 
@@ -135,11 +131,10 @@ while True:
         print('Se encontraron: ', book_count, ' Libros')
 
     elif int(inputs[0]) == 5:
-        # TODO completar modificaciones para el laboratorio 4
         size = input("Indique tamaño de la muestra: ")
         result = controller.sortBooks(catalog, int(size))
-        print("Para la muestra de", size, " elementos, el tiempo (mseg) es: ",
-                                          str(result))
+        print("Para la muestra de", size, " elementos, el tiempo (mseg) es: ", str(result[0]))
+        printSortResults(result[1])
 
     else:
         sys.exit(0)
